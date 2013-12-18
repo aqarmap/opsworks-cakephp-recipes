c58f8d62d8527c425fa224e98448b23d5482af39
Khaled Attia <khaledattia@gmail.com>
2013-12-18 20:15:15 +0200

    Added database recipe

---
+++ cakephp/recipes/database.rb
@@ -0,0 +1,19 @@
+node[:deploy].each do |application, deploy|
+	if deploy[:application_type] != 'php'
+		Chef::Log.debug("Skipping cakephp::database application #{application} as it is not an PHP app")
+		next
+	end
+
+	template "#{deploy[:deploy_to]}/current/app/Config/database.php" do
+		source "database.php.erb"
+		owner "root"
+		group "root"
+		mode "0644"
+		variables(
+			:db_host		=> (deploy[:database][:host] rescue nil),
+			:db_name		=> (deploy[:database][:database] rescue nil),
+			:db_user		=> (deploy[:database][:username] rescue nil),
+			:db_password	=> (deploy[:database][:password] rescue nil)
+		)
+	end
+end
